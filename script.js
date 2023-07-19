bace047266e962df70397b185fe9984acded4d39
Aayush <78372874+aayush180@users.noreply.github.com>
2023-07-19 20:33:33 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const ImgContainer = document.querySelector(".imgContainer");
+const Info = document.querySelector(".info");
+const AnimText1 = document.querySelector(".anim-text1");
+const AnimText2 = document.querySelector(".anim-text2");
+
+
+
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
+
+window.addEventListener("scroll",() =>{
+   let offSetY = window.scrollY;
+   ImgContainer.style.transform = `translateY(${offSetY * 0.1}px)`
+   Info.style.transform = `translate(${offSetY * 0.3}px,${offSetY * 0.1}px)`
+   AnimText1.style.transform = `translateX(${offSetY * 1.4}px)`
+   AnimText2.style.transform = `translateX(calc(210vh - ${offSetY}px))`
+})
+
+
+
+
